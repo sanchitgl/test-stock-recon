c71dfdebe682768acc5796fdd284b11b4f0bac56
sanchitgoel7 <105559691+sanchitgoel7@users.noreply.github.com>
2022-07-06 15:35:36 +0530

    UI style update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import yaml
 import altair as alt
 from PIL import Image
 import time
+import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
@@ -42,6 +43,23 @@ if authentication_status == None:
 if authentication_status:
     time.sleep(1)
     def landing_page():
+    # Remove whitespace from the top of the page and sidebar
+        st.markdown("""
+                <style>
+                    .css-18e3th9 {
+                            padding-top: 0rem;
+                            padding-bottom: 10rem;
+                            padding-left: 5rem;
+                            padding-right: 5rem;
+                        }
+                    .css-1d391kg {
+                            padding-top: 3.5rem;
+                            padding-right: 1rem;
+                            padding-bottom: 3.5rem;
+                            padding-left: 1rem;
+                        }
+                </style>
+                """, unsafe_allow_html=True)
         logo = Image.open('images/reconcify_logo.png')
         hide_streamlit_style = """
             <style>
@@ -50,11 +68,13 @@ if authentication_status:
             </style>
             """
         st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-        emp,Logo,emp = st.columns([2,2,2])
+        Logo,emp = st.columns([1.5,4])
         with Logo:
             st.image(logo)
         #with title:
-        #st.header('Shipment Reconcilliation')
+        emp,title,emp = st.columns([2,2,2])
+        with title:
+            st.header('Shipment Reconciliation')
         #st.write('###')
         shipment_instructions, warehouse_reports, inventory_ledger, submit = file_upload_form()
         #print(warehouse_reports)
@@ -83,14 +103,15 @@ if authentication_status:
             pie_data = [['Matching SKUs',matching_sku],['Mismatching SKUs', mismatching_sku]]
             pie_df = pd.DataFrame(pie_data, columns=['Label', 'Units']) 
             #pie_df = pie_df.set_index('Label')    
-            with st.expander('View KPI Charts'):
+            with st.expander('Visualize Reconciliation Output'):
                 bar,pie = st.columns([1.2,1]) 
                 with bar:
-                    plot_bar_chart(bar_df,'Label','Units')
+                    plot_waterfall_chart(units_booked, excess_units_received, short_units_received, units_received)
+                    #plot_bar_chart(bar_df,'Label','Units')
                     #st.bar_chart(bar_df)
                 with pie:
                     #st.bar_chart(pie_df)
-                    plot_pie_chart(pie_df,'Label','Units')
+                    plot_pie_chart(matching_sku, mismatching_sku)
             with open('temp/shipment_reco.xlsx', 'rb') as my_file:
                 click = st.download_button(label = 'Download in Excel', data = my_file, file_name = 'shipment_reco.xlsx', 
                 mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', on_click = delete_temp()) 
@@ -117,13 +138,48 @@ if authentication_status:
         
         st.altair_chart(chart, use_container_width=True)
 
-    def plot_pie_chart(data,X,Y):
-        pie = alt.Chart(data).mark_arc(innerRadius=50).encode(
-        theta=alt.Theta(field=Y, type="quantitative"),
-        color=alt.Color(field=X, type="nominal",legend=alt.Legend(orient="bottom",title = "",padding= 20)),
-        tooltip = [alt.Tooltip(Y, title="",format='.1f')]
+    def plot_waterfall_chart(units_booked, excess_units_received, short_units_received, units_received):
+        fig = go.Figure(go.Waterfall(
+            name = "20", orientation = "v",
+            measure = ["relative", "relative", "relative", "total"],
+            x = ["Units Booked", "Excess Units", "Short Units", "Units Recieved"],
+            textposition = "outside",
+            text = [str(units_booked), str(excess_units_received), str(short_units_received), str(units_received)],
+            y = [units_booked, excess_units_received, short_units_received, units_received],
+            decreasing = {"marker":{"color":"#E97451"}},
+            increasing = {"marker":{"color":"#40B5AD"}},
+            totals = {"marker":{"color":"#6F8FAF"}},
+            connector = {"line":{"color":"rgb(63, 63, 63)"}},
+        ))
+
+        fig.update_layout(
+                title = 'Reconciliation Movement', title_x=0.5,
+                showlegend = False
         )
-        st.altair_chart(pie, use_container_width=True)
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    def plot_pie_chart(matching_sku, mismatching_sku):
+        labels = ['Matching SKUs','Mismatching SKUs']
+        values = [matching_sku, mismatching_sku]
+        
+        mark_colours = ['#6F8FAF','#E97451']
+
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3, marker_colors=mark_colours)])
+        
+
+        fig.update_layout(
+                title = 'Reconciliation Split',title_x=0.42
+                #showlegend = False
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+        # pie = alt.Chart(data,title='Reconcilliation Split').configure_title(fontSize=20).mark_arc(innerRadius=50).encode(
+        # theta=alt.Theta(field=Y, type="quantitative"),
+        # color=alt.Color(field=X, type="nominal",legend=alt.Legend(orient="bottom",title = "")),
+        # tooltip = [alt.Tooltip(Y, title="",format='.1f')]
+        # )
+        # st.altair_chart(pie)
 
     def delete_temp():
         os.remove("temp/shipment_reco.xlsx")
